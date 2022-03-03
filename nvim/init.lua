09b41496a35b320491e3b1c492f456a60ca58d4d
Daan van der Kallen <mail@daanvdk.com>
2022-03-03 15:50:50 +0100

    Add fzf to fish and zls to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -153,6 +153,7 @@ local servers = {
         flake8 = { enabled = true }
     },
     tsserver = {},
+    zls = {},
 }
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
